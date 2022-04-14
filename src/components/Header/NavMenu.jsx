25d21cc361ad0a72916f329b835d21c1b99bd2b4
tibononoX <thibault-carre@hotmail.fr>
2022-04-14 16:25:46 +0200

    almost finalised header for mobile and desktop

--- src/components/Header/NavMenu.jsx
+++ src/components/Header/NavMenu.jsx
@@ -1,52 +1,89 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./NavMenu.scss";
+import Logo from "./Logo";
 
-const NavMenu = () => {
+const NavMenu = ({ isScrolled }) => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
 
   return (
     <nav className="navMenu">
-      {isOpen ? (
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="navButton translateLeft rotate90"
-        >
-          <div className="menuLogoBars cross"></div>
-          <div className="menuLogoBars crossInverted"></div>
-          <div className="menuLogoBars hide"></div>
-        </button>
-      ) : (
-        <button onClick={() => setIsOpen(!isOpen)} className="navButton">
-          <div className="menuLogoBars"></div>
-          <div className="menuLogoBars"></div>
-          <div className="menuLogoBars"></div>
-        </button>
-      )}
+      <div className="mobile">
+        {isOpen ? (
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            type="button"
+            className="navButton rotate90"
+          >
+            <span className="menuLogoBars cross" />
+            <span className="menuLogoBars crossInverted" />
+            <span className="menuLogoBars hide" />
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            type="button"
+            className="navButton"
+          >
+            <span className="menuLogoBars" />
+            <span className="menuLogoBars" />
+            <span className="menuLogoBars" />
+          </button>
+        )}
 
-      <ul className={`navList ${isOpen && "opened"}`}>
-        <li>
-          <Link onClick={() => setIsOpen(false)} to="/">
-            Homepage
+        <ul className={`navList ${isOpen && "opened"}`}>
+          <li>
+            <Link onClick={() => setIsOpen(false)} to="/">
+              Homepage
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setIsOpen(false)} to="/search">
+              Search
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setIsOpen(false)} to="/history">
+              History
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setIsOpen(false)} to="/jobs">
+              Jobs
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {isScrolled ? (
+        <div className="desktopScroll">
+          <div className="leftButtons">
+            <Link to="/">HOMEPAGE</Link>
+            <Link to="/search">SEARCH</Link>
+          </div>
+          <Link to="/" className="midLogo">
+            <Logo isScrolled={isScrolled} />
           </Link>
-        </li>
-        <li>
-          <Link onClick={() => setIsOpen(false)} to="/search">
-            Search
+          <div className="rightButtons">
+            <Link to="/history">HISTORY</Link>
+            <Link to="/jobs">JOBS</Link>
+          </div>
+        </div>
+      ) : (
+        <div className="desktop">
+          <div className="leftButtons">
+            <Link to="/">HOMEPAGE</Link>
+            <Link to="/search">SEARCH</Link>
+          </div>
+          <Link to="/" className="midLogo">
+            <Logo isScrolled={isScrolled} />
           </Link>
-        </li>
-        <li>
-          <Link onClick={() => setIsOpen(false)} to="/history">
-            History
-          </Link>
-        </li>
-        <li>
-          <Link onClick={() => setIsOpen(false)} to="/jobs">
-            Jobs
-          </Link>
-        </li>
-      </ul>
+          <div className="rightButtons">
+            <Link to="/history">HISTORY</Link>
+            <Link to="/jobs">JOBS</Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
