7f7336ca3d818ecc4a6cae0921edcd9142ec0561
tibononoX <thibault-carre@hotmail.fr>
2022-04-19 12:36:52 +0200

    started adding style to the footer

--- frontend/src/components/Footer/FooterLogo.jsx
+++ frontend/src/components/Footer/FooterLogo.jsx
@@ -1,5 +1,4 @@
 import "@assets/fonts/BTTF.ttf";
-import { useState } from "react";
 import "./FooterLogo.css";
 
 const FooterLogo = ({ isMobile }) => {
@@ -16,20 +15,19 @@ const FooterLogo = ({ isMobile }) => {
         </div>
       </div>
     );
-  } else {
-    return (
-      <div className="logo-container-footer fontBTTF">
-        <div className="logo-footer">
-          <span className="top-line flicker">BACK{"<"} </span>
-          <span className="bottom-line flicker margin-8">& BAR</span>
-        </div>
-        <div className="logo-footer">
-          <span className="gradient-text">BACK{"<"} </span>
-          <span className="margin-8 gradient-text">& BAR</span>
-        </div>
-      </div>
-    );
   }
+  return (
+    <div className="logo-container-footer fontBTTF">
+      <div className="logo-footer">
+        <span className="top-line flicker">BACK{"<"} </span>
+        <span className="bottom-line flicker margin-8">& BAR</span>
+      </div>
+      <div className="logo-footer">
+        <span className="gradient-text">BACK{"<"} </span>
+        <span className="margin-8 gradient-text">& BAR</span>
+      </div>
+    </div>
+  );
 };
 
 export default FooterLogo;
