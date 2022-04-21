176c523e043ffcc5e6ccc525d2b5a06cd76d1b94
tibononoX <thibault-carre@hotmail.fr>
2022-04-21 17:30:11 +0200

    started structuring search page

---
+++ frontend/src/components/SearchPage/SearchWindow.jsx
@@ -0,0 +1,28 @@
+import "@components/SearchPage/SearchWindow.scss";
+
+const SearchWindow = () => {
+  return (
+    <section className="searchContainer">
+      <div className="searchBar">
+        <ul className="searchList">
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
+      </div>
+      <div className="cardsContainer">
+        <span className="card" />
+        <span className="card" />
+        <span className="card" />
+        <span className="card" />
+        <span className="card" />
+        <span className="card" />
+        <span className="card" />
+      </div>
+    </section>
+  );
+};
+
+export default SearchWindow;
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,11 +1,13 @@
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
+import SearchWindow from "@components/SearchPage/SearchWindow";
 
 const Search = () => {
   return (
     <>
-      <section className="topPage">
+      <section className="topPage column">
         <Header />
+        <SearchWindow />
       </section>
       <section className="midPage">
         <p>search</p>
