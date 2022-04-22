58c5ee5f2798e7a217b2bd2c2dbd414944ab49f3
tibononoX <thibault-carre@hotmail.fr>
2022-04-22 12:27:24 +0200

    integrated basic carrousel

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
+import Carrousel from "@components/Carrousel/Carrousel";
 // import { NeonH, NeonV } from "@components/NeonSeparateur/NeonSeparateur";
 // import { Cards, CardsOff } from "@components/Carrousel/Cartes";
 
@@ -8,6 +9,9 @@ const Homepage = () => {
     <>
       <section className="topPage">
         <Header />
+        <div className="carrous">
+          <Carrousel />
+        </div>
       </section>
       <section className="midPage">
         <p>homepage</p>
