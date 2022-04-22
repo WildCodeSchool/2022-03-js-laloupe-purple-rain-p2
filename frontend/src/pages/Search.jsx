2ead5904d9d7f2a0209f8acacf9e682c7c4222c5
Damien-Boukourida <IceRaag.GMG@gmail.com>
2022-04-22 09:34:39 +0200

    minor fix 2

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
-import { NeonH, NeonV } from "@components/NeonSeparateur/NeonSeparateur";
+// import { NeonH, NeonV } from "@components/NeonSeparateur/NeonSeparateur";
 import { Cards, CYC } from "@components/Carrousel/Cartes";
 
 const Search = () => {
@@ -15,7 +15,7 @@ const Search = () => {
         <Cards />
         <Cards />
       </section>
-      <section className="midPage"></section>
+      <section className="midPage" />
       <section className="botPage">
         <Footer />
       </section>
