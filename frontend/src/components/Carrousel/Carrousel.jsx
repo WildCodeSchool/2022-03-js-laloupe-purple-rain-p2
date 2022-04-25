9b8adfcf6d936312810c8bd70b9cd72b88b72b9d
Kuroshin28 <baptiste.bertinWOT@gmail.com>
2022-04-25 12:01:29 +0200

    Modify Carrousel.jsx :
    - Allow to switch to desktop and mobile version of carrousel due to break point set at 1024px (width)
    - Apply transform when trigger with ternary.
    - No automatic settings for , maybe improve the code.
    - /!\ Random button need to be moved somewhere else in order to properly display.
    
    TO DO : reset the card position of the carrousel when break events.

--- frontend/src/components/Carrousel/Carrousel.jsx
+++ frontend/src/components/Carrousel/Carrousel.jsx
@@ -10,8 +10,6 @@ const tableOfIndex = [0, 1, 2, 3];
 function scaleWidth(width) {
   if (width < 600) {
     let ratio = 1 - (0.8 * (600 - width)) / 600;
-    //console.log(width);
-    //console.log(ratio);
     return ratio;
   }
   return 1;
@@ -19,39 +17,38 @@ function scaleWidth(width) {
 
 function translateZAxis(index) {
   const middle = 2;
+  const zOffset = -300;
   const Axe1 = index / middle;
   const Axe2 = index % middle;
 
   if (Axe1 !== 0 && Axe2 !== 0) {
-    return (middle - Axe2) * -300;
+    return (middle - Axe2) * zOffset;
   } else if (Axe1 !== 1 && Axe2 !== 0) {
-    return Axe2 * -300;
+    return Axe2 * zOffset;
   } else if (Axe1 !== 0 && Axe2 !== 1) {
-    return middle * -300;
+    return middle * zOffset;
   }
   return 0;
 }
 
 function translateYAxis(index) {
   const middle = 2;
+  const yOffset = 37.5;
   const Axe1 = index / middle;
   const Axe2 = index % middle;
 
   if (Axe1 !== 0 && Axe2 != 0) {
-    return (middle - Axe2) * 37.5;
+    return (middle - Axe2) * yOffset;
   } else if (Axe1 !== 1 && Axe2 !== 0) {
-    return Axe2 * 37.5;
+    return Axe2 * yOffset;
   } else if (Axe1 !== 0 && Axe2 !== 1) {
-    return middle * 37.5;
+    return middle * yOffset;
   }
   return 0;
 }
 
 function Carrousel() {
   const [touchPosition, setTouchPosition] = React.useState(null);
-  const [currentWindowWidth, setCurrentWindowWidth] = useState(
-    window.innerWidth
-  );
 
   function updateWindowSize() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -59,7 +56,6 @@ function Carrousel() {
     useEffect(() => {
       const handleWindowWidth = () => {
         setWindowWidth(window.innerWidth);
-        setCurrentWindowWidth(windowWidth);
       };
 
       window.addEventListener("resize", handleWindowWidth);
@@ -86,12 +82,10 @@ function Carrousel() {
     const currentTouch = e.touches[0].clientX;
     const diff = touchHold - currentTouch;
     if (diff > 5) {
-      console.log("Touch move to right deepshit !");
       const first = tableOfIndex.pop();
       tableOfIndex.unshift(first);
     }
     if (diff < -5) {
-      console.log("Touch move to left deepshit !");
       const last = tableOfIndex.shift();
       tableOfIndex.push(last);
     }
@@ -115,53 +109,66 @@ function Carrousel() {
             className="Carte Bleue Carrousel"
             id="0"
             style={
-              currentWindowWidth <= 1024
+              width <= 1024
                 ? {
-                    transform: `translateX(${
-                      Math.sin((tableOfIndex[0] * Math.PI) / 2) * 150
+                  transform: `translateX(${Math.sin((tableOfIndex[0] * Math.PI) / 2) * 150
                     }px) translateY(${translateYAxis(
                       tableOfIndex[0]
                     )}px) translateZ(${translateZAxis(tableOfIndex[0])}px`,
-                    TransitionEvent: `all 1s`,
-                  }
-                : { transform: `none` }
+                  TransitionEvent: `all 1s`,
+                }
+                : { transform: `translateY(-20px)` }
             }
           ></figure>
           <figure
             className="Carte Verte Carrousel"
-            id="cartomancie"
-            style={{
-              transform: `translateX(${
-                Math.sin((tableOfIndex[1] * Math.PI) / 2) * 150
-              }px) translateY(${translateYAxis(
-                tableOfIndex[1]
-              )}px) translateZ(${translateZAxis(tableOfIndex[1])}px)`,
-              TransitionEvent: `all 1s`,
-            }}
+            id="1"
+            style={
+              width <= 1024 ?
+                {
+                  transform: `translateX(${Math.sin((tableOfIndex[1] * Math.PI) / 2) * 150
+                    }px) translateY(${translateYAxis(
+                      tableOfIndex[1]
+                    )}px) translateZ(${translateZAxis(tableOfIndex[1])}px)`,
+                  TransitionEvent: `all 1s`,
+                }
+                : {
+                  transform: `translateX(350px) translateY(40px) rotate(20deg)`,
+                  TransitionEvent: `all 1s`,
+                }}
           ></figure>
           <figure
             className="Carte Rose Carrousel"
             id="2"
-            style={{
-              transform: `translateX(${
-                Math.sin((tableOfIndex[2] * Math.PI) / 2) * 150
-              }px) translateY(${translateYAxis(
-                tableOfIndex[2]
-              )}px) translateZ(${translateZAxis(tableOfIndex[2])}px)`,
-              TransitionEvent: `all 1s`,
-            }}
+            style={
+              width <= 1024 ?
+                {
+                  transform: `translateX(${Math.sin((tableOfIndex[2] * Math.PI) / 2) * 150
+                    }px) translateY(${translateYAxis(
+                      tableOfIndex[2]
+                    )}px) translateZ(${translateZAxis(tableOfIndex[2])}px)`,
+                  TransitionEvent: `all 1s`,
+                }
+                : {
+                  display: "none"
+                }}
           ></figure>
           <figure
             className="Carte Orange Carrousel"
             id="3"
-            style={{
-              transform: `translateX(${
-                Math.sin((tableOfIndex[3] * Math.PI) / 2) * 150
-              }px) translateY(${translateYAxis(
-                tableOfIndex[3]
-              )}px) translateZ(${translateZAxis(tableOfIndex[3])}px)`,
-              TransitionEvent: `all 1s`,
-            }}
+            style={
+              width <= 1024 ?
+                {
+                  transform: `translateX(${Math.sin((tableOfIndex[3] * Math.PI) / 2) * 150
+                    }px) translateY(${translateYAxis(
+                      tableOfIndex[3]
+                    )}px) translateZ(${translateZAxis(tableOfIndex[3])}px)`,
+                  TransitionEvent: `all 1s`,
+                }
+                : {
+                  transform: `translateX(-350px) translateY(40px) rotate(-20deg)`,
+                  TransitionEvent: `all 1s`,
+                }}
           ></figure>
         </section>
       </section>
