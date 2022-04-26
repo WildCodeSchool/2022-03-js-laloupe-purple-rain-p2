dd5234fa8e66d35757ad5d79f321a3214f4d6a7c
tibononoX <thibault-carre@hotmail.fr>
2022-04-26 17:31:54 +0200

    added functional filter by number and fixed some bugs, also commented some of the code

--- frontend/src/components/SearchPage/SearchWindow.jsx
+++ frontend/src/components/SearchPage/SearchWindow.jsx
@@ -19,94 +19,6 @@ const cocktailDataRaw = [
     strIngredient4: "",
     strIngredient5: "",
   },
-  {
-    strDrink: "Gin Lemon",
-    strCategory: "Cocktail",
-    strAlcoholic: "Alcoholic",
-    strGlass: "Highball glass",
-    strInstructions:
-      "For the preparation of the gin lemon you will not need the shaker. Fill the tumbler with ice, pour the gin and lemonade over it. Gently mix and decorate with a slice of lemon. Those who prefer can also add a few mint leaves. Your gin lemon is ready to be served.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/yhzvk91643821739.jpg",
-    strIngredient1: "Gin",
-    strIngredient2: "Lemon Juice",
-    strIngredient3: "Lemon Peel",
-    strIngredient4: "Ice",
-    strIngredient5: "",
-  },
-  {
-    strDrink: "Whitecap Margarita",
-    strCategory: "Other/Unknown",
-    strAlcoholic: "Alcoholic",
-    strGlass: "Margarita/Coupette glass",
-    strInstructions:
-      "Place all ingredients in a blender and blend until smooth. This makes one drink.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg",
-    strIngredient1: "Ice",
-    strIngredient2: "Tequila",
-    strIngredient3: "Cream of coconut",
-    strIngredient4: "Lime juice",
-    strIngredient5: "",
-  },
-  {
-    strDrink: "Spanish chocolate",
-    strCategory: "Cocoa",
-    strAlcoholic: "Non alcoholic",
-    strGlass: "Coffee mug",
-    strInstructions:
-      "Stir the milk with the chocolate and the cinnamon over low heat until the chocolate dissolves. Add the eggs and beat the mixture until it becomes thick, taking care not to boil. Serve in coffee mug.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/pra8vt1487603054.jpg",
-    strIngredient1: "Milk",
-    strIngredient2: "Chocolate",
-    strIngredient3: "Cinnamon",
-    strIngredient4: "Egg yolk",
-    strIngredient5: "",
-  },
-  {
-    strDrink: "Bacardi Cocktail",
-    strCategory: "Ordinary Drink",
-    strAlcoholic: "Alcoholic",
-    strGlass: "Cocktail glass",
-    strInstructions: "Shake together with ice. Strain into glass and serve.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/n433t21504348259.jpg",
-    strIngredient1: "Light rum",
-    strIngredient2: "Lime juice",
-    strIngredient3: "Sugar syrup",
-    strIngredient4: "Grenadine",
-    strIngredient5: "",
-  },
-  {
-    strDrink: "Holloween Punch",
-    strCategory: "Punch / Party Drink",
-    strAlcoholic: "Non alcoholic",
-    strGlass: "Punch bowl",
-    strInstructions:
-      'Take a bunch of grape juice and a bunch of fizzy stuff (club soda, ginger ale, lemonlime, whatever). Mix them in a punch bowl. Take orange sherbet and lime sherbet. Scoop out scoops and float them in the punch, let them melt a little so that a nasty film spreads all over the top of the punch but there are still "bubbles" in it in the form of sherbet scoops. Looks horrible, tastes just fine.',
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/lfeoe41504888925.jpg",
-    strIngredient1: "Grape juice",
-    strIngredient2: "Carbonated soft drink",
-    strIngredient3: "Sherbet",
-    strIngredient4: "Sherbet",
-    strIngredient5: "",
-  },
-  {
-    strDrink: "Fruit Shake",
-    strCategory: "Other/Unknown",
-    strAlcoholic: "Non alcoholic",
-    strGlass: "Highball Glass",
-    strInstructions: "Blend til smooth.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/q0fg2m1484430704.jpg",
-    strIngredient1: "Yoghurt",
-    strIngredient2: "Banana",
-    strIngredient3: "Orange juice",
-    strIngredient4: "Fruit",
-    strIngredient5: "Ice",
-  },
 ];
 
 const letterBar = () => {
@@ -119,19 +31,28 @@ const letterBar = () => {
 };
 
 const SearchWindow = () => {
-  const [cocktailList, setCocktailList] = useState(cocktailDataRaw);
-  const [searchField, setSearchField] = useState("");
-  const [currentLetter, setCurrentLetter] = useState("a");
+  const [searchField, setSearchField] = useState(""); // Search field input value
+  const [cocktailList, setCocktailList] = useState(cocktailDataRaw); // Actual list of drinks
+  const [numberFiltered, setNumberFiltered] = useState(false); // Drinks names starts by a number
+  const [currentLetter, setCurrentLetter] = useState(""); // Current filter by letter
+  const [filtered, setFiltered] = useState(false); // Is there any filter active ?
+
   const [maxItem, setMaxItem] = useState(10);
   const [minItem, setMinItem] = useState(0);
 
-  const resetPage = () => {
+  // Goes to "first page" by setting array offset to 0
+  const firstPage = () => {
     setMinItem(0);
     setMaxItem(10);
   };
-
+  // Goes to "last page" by setting array offset to array.length
+  const lastPage = () => {
+    setMinItem(cocktailList.length - 10);
+    setMaxItem(cocktailList.length);
+  };
+  // Offsets display of array by -10 after checking if possible (first page won't offset)
   const prevPage = () => {
-    if (minItem === 0) {
+    if (minItem <= 10) {
       setMinItem(0);
       setMaxItem(10);
     } else {
@@ -139,8 +60,9 @@ const SearchWindow = () => {
       setMaxItem(maxItem - 10);
     }
   };
+  // Offsets display of array by +10 after checking if possible (last page won't offset)
   const nextPage = () => {
-    if (maxItem === cocktailList.length - (cocktailList.length % 10)) {
+    if (maxItem >= cocktailList.length - (cocktailList.length % 10)) {
       setMinItem(cocktailList.length - 10);
       setMaxItem(cocktailList.length);
     } else {
@@ -149,36 +71,80 @@ const SearchWindow = () => {
     }
   };
 
-  // const setPageNumber = () => {
-  //   let pageNumber = 1;
-
-  //   if (cocktailList.length % 10 === 0) {
-  //     console.log("isEven", cocktailList.length);
-  //   } else {
-  //     console.log("isOdd", cocktailList.length);
-  //   }
-
-  //   // console.log(pageNumber);
-  //   // return pageNumber;
-  // };
-
-  async function getDrinkByLetter(letter) {
+  // Fetches all drinks from API using "?s=" without any parameters, kinda weird ut it works
+  async function getAllDrinks() {
     const response = await axios
-      .get("https://www.thecocktaildb.com/api/json/v2/9973533/search.php", {
-        params: { f: letter },
-      })
-      .then((data) => data.data.drinks);
+      .get("https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=")
+      .then((data) => data.data.drinks)
+      .catch((err) => {
+        console.error(err);
+      });
 
     if (response) {
-      // setPageNumber();
-      resetPage();
+      firstPage();
       return response;
     }
     return [];
   }
 
+  // Fetches the whole drink lists and apply a filters to keep only the ones with names starting by a number
+  const startsWithNumber = () => {
+    getAllDrinks().then((data) =>
+      setCocktailList(
+        data.filter(
+          (item) =>
+            item.strDrink.charCodeAt(0) >= 48 &&
+            item.strDrink.charCodeAt(0) <= 57
+        )
+      )
+    );
+    setNumberFiltered(true);
+    setFiltered(true);
+  };
+
+  // Fetches API with "?f=" parameter to recover only drinks starting with the specified letter
+  async function getDrinkByLetter(letter) {
+    const response = await axios
+      .get("https://www.thecocktaildb.com/api/json/v2/9973533/search.php", {
+        params: { f: letter },
+      })
+      .then((data) => data.data.drinks)
+      .catch((err) => {
+        console.error(err);
+      });
+
+    if (response) {
+      firstPage();
+      setFiltered(true);
+      return response;
+    }
+    return [];
+  }
+
+  // Resets filters to display all drinks
+  const resetFilters = () => {
+    if (currentLetter !== "") {
+      setCurrentLetter("");
+    }
+    if (numberFiltered) {
+      setNumberFiltered(false);
+    }
+    setFiltered(false);
+  };
+
+  // Checks filtering state and handles letter filtering requests
   useEffect(() => {
-    getDrinkByLetter(currentLetter).then((data) => setCocktailList(data));
+    if (currentLetter === "" && numberFiltered === false) {
+      setFiltered(false);
+    } else {
+      setFiltered(true);
+    }
+
+    if (currentLetter === "" && numberFiltered === false) {
+      getAllDrinks().then((data) => setCocktailList(data));
+    } else {
+      getDrinkByLetter(currentLetter).then((data) => setCocktailList(data));
+    }
   }, [currentLetter]);
 
   /* eslint-disable */
@@ -201,15 +167,27 @@ const SearchWindow = () => {
       </div>
 
       <ul className="letterBar">
-        <p className="counter">Drinks: {cocktailList.length}</p>
+        <p className="counter">{cocktailList.length} results</p>
+        <p
+          className={filtered ? "rmFilters" : "hide"}
+          onClick={() => resetFilters()}
+        >
+          Remove Filters
+        </p>
         {letterBar().map((letter) => {
-          return <li onClick={() => setCurrentLetter(letter)}>{letter}</li>;
+          return (
+            <li onClick={() => setCurrentLetter(letter)} key={letter}>
+              {letter}
+            </li>
+          );
         })}
-        <li>0-1</li>
+        <li onClick={() => startsWithNumber()}>0-9</li>
       </ul>
       <ul className="pageBar">
+        <li onClick={() => firstPage()}>{`<!`}</li>
         <li onClick={() => prevPage()}>{`<-`}</li>
         <li onClick={() => nextPage()}>{`->`}</li>
+        <li onClick={() => lastPage()}>{`!>`}</li>
       </ul>
 
       <div className="cardsContainer">
@@ -220,12 +198,14 @@ const SearchWindow = () => {
           .slice(minItem, maxItem)
           .map((item) => {
             // eslint-disable-next-line
-            return <Cards {...item} />;
+            return <Cards {...item} key={item.idDrink} />;
           })}
       </div>
       <ul className="pageBar">
+        <li onClick={() => firstPage()}>{`<!`}</li>
         <li onClick={() => prevPage()}>{`<-`}</li>
         <li onClick={() => nextPage()}>{`->`}</li>
+        <li onClick={() => lastPage()}>{`!>`}</li>
       </ul>
     </section>
   );
