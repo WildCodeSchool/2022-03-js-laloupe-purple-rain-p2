fc60671ce2f72f94f187296270cd76c257203a3e
tibononoX <thibault-carre@hotmail.fr>
2022-05-09 17:58:33 +0200

    adjusted inner neon glow for cards for better visuals
    enhanced light theme compatibility of cards on mobile and desktop

--- frontend/src/components/SearchPage/Cartes.jsx
+++ frontend/src/components/SearchPage/Cartes.jsx
@@ -1,16 +1,32 @@
+import LightThemeContext from "@contexts/LightTheme";
+import { useContext } from "react";
 import "./Cartes.scss";
 
 const Cards = ({ setInfoPopup, ...item }) => {
+  const { lightTheme } = useContext(LightThemeContext);
+
   const handleNeonColor = () => {
     if (item.strCategory === "Cocktail") {
+      if (lightTheme) {
+        return "Carte Bleue light";
+      }
       return "Carte Bleue";
     }
     if (item.strCategory === "Ordinary Drink") {
+      if (lightTheme) {
+        return "Carte Rose light";
+      }
       return "Carte Rose";
     }
 
     if (item.strCategory === "Other/Unknown") {
+      if (lightTheme) {
+        return "Carte Orange light";
+      }
       return "Carte Orange";
+    }
+    if (lightTheme) {
+      return "Carte Verte light";
     }
     return "Carte Verte";
   };
