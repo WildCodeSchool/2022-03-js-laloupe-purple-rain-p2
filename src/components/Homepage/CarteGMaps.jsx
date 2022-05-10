894b3d24c272c1996766d743358150a547d1e2a2
Damien-Boukourida <IceRaag.GMG@gmail.com>
2022-05-10 16:25:55 +0200

    Ajout api google maps

---
+++ src/components/Homepage/CarteGMaps.jsx
@@ -0,0 +1,28 @@
+import "./CarteGMaps.scss";
+import HomeGoogleMaps from "./Maps";
+import LightThemeContext from "@contexts/LightTheme";
+import { useContext } from "react";
+
+const Maps = () => {
+  const { lightTheme } = useContext(LightThemeContext);
+
+  return (
+    <div
+      className={lightTheme ? "formationContainer light" : "formationContainer"}
+    >
+      <div className="MapComponent">
+        <div className="GMaps">
+          <HomeGoogleMaps />
+          <div className="Txt">
+            <p>
+              Need to relax alone or with your friends ? Look at the list of
+              bars nearby.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Maps;
